4f29b86b36f42bfcf1b60600709505dbcd7cbbce
trazip <p.penhard@gmail.com>
2025-09-07 15:32:05 +0200

    Remove cover letter job and add ruby_llm gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "3.1.2"
+ruby "3.3.5"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.5", ">= 7.1.5.1"
@@ -28,9 +28,9 @@ gem "cssbundling-rails"
 
 gem "devise", "~> 4.9"
 
-gem "ruby-openai"
+gem 'ruby_llm', '~> 1.6', '>= 1.6.4'
 
-gem 'solid_queue', '~> 1.1', '>= 1.1.4'
+# gem 'solid_queue', '~> 1.1', '>= 1.1.4'
 gem 'solid_cable'
 
 # Use Redis adapter to run Action Cable in production
--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -6,6 +6,40 @@ class CoverLettersController < ApplicationController
   end
 
   def create
-    CoverLetterGeneratorJob.perform_later(params[:position], params[:company], params[:job_description], current_user.id)
+    position = params[:position]
+    company = params[:company]
+    job_description = params[:job_description]
+    user_id = current_user.id
+
+    # Broadcast skeleton immediately
+    Turbo::StreamsChannel.broadcast_update_to(
+      "user_#{user_id}_cover_letter_form",
+      target: "cover_letter_form",
+      partial: "cover_letters/skeleton"
+    )
+
+    instructions = <<~TEXT
+      I'm applying to a #{position} position at #{company}
+      and I need you to generate a cover letter for me. Here is the
+      job description: #{job_description}
+
+      Maximum length should be 200 words. The form should have some spaces
+      between the introduction and the body of the letter.
+    TEXT
+
+    content = ""
+    
+    RubyLLM.chat.with_instructions(instructions).ask("Generate a cover letter") do |chunk|
+      next if chunk.content.blank? # skip empty chunks
+      
+      content += chunk.content
+      Turbo::StreamsChannel.broadcast_update_to(
+        "user_#{user_id}_cover_letter_form",
+        target: "cover_letter_form",
+        partial: "cover_letters/cover_letter",
+        locals: { content: content }
+      )
+    end
   end
+
 end
